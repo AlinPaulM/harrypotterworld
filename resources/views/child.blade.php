c8fccb103613442a7f6f70b0f9faae0e96024ffe
Alin M <macoveialinp@gmail.com>
2022-04-11 20:02:35 +0100

    learning template inheritance

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -1,11 +1,12 @@
 {{--
-/* 
 INFO
 -----
 @extends('layoutName')                                          - extends a layout
 @section('sectionName') [section content] @endsection           - displays [section content] (overwrites parent content)
 @section('sectionName') @parent [section content] @endsection   - displays @parent content and [section content]
-*/
+    -P.S. eu: sections that are defined in the parent template with @hasSection, should NOT use @parent in the child template - it does NOT work properly(if your child section's content is only '@parent', then nothing will be displayed; the order of content vs '@parent' is not respected; etc)
+
+P.S. ALWAYS check the rendered DOM - if you use these directives wrong, it all gets messed up
 --}}
 
 @extends('layouts.app')
@@ -19,4 +20,8 @@ INFO
 
 @section('content')
     <p>body content.</p>
+@endsection
+
+@section('childSection')
+<p>hasSection child content</p>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,10 @@
 {{--
-/*
 INFO
 -----
 @section('sectionName') ... @endsection - defines a section
 @section('sectionName') ... @show       - defines + yields a section
 @yield('sectionName')                   - yields(displays) a section
 @yield('sectionName', 'default content')- yields 'default content' if the section 'sectionName' is undefined
-*/
 --}}
 <html>
     <head>
@@ -20,5 +18,17 @@ INFO
         <div class="container">
             @yield('content')
         </div>
+
+        {{--https://laracasts.com/discuss/channels/laravel/blade-hassection-what-is-it-really--}}
+        @hasSection('childSection')
+            <br>
+            <p>hasSection parent content</p>
+            @yield('childSection')
+        @endif
+        
+        @sectionMissing('childSectionMissing')
+            <br>
+            <p>childSectionMissing, so here's some default content</p>
+        @endif
     </body>
 </html>
